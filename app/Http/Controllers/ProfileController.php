1d5439ef0abefe5d5878e90288f8937c7059e403
Thuan Bui <9248622+10h30@users.noreply.github.com>
2025-04-03 21:42:12 +0900

    complete all tasks

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Password;
 
 class ProfileController extends Controller
 {
@@ -11,10 +15,27 @@ class ProfileController extends Controller
         return view('auth.profile');
     }
 
+
     public function update(ProfileUpdateRequest $request)
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+        $validated = request()->validate([
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', Rule::unique('users')->ignore(Auth::user())],
+            'password' => ['sometimes', 'nullable', 'confirmed', Password::defaults()],
+        ]);
+        //dd($validated);
+        Auth::user()->update([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+        ]);
+
+        if (isset($validated['password'])) {
+            Auth::user()->update([
+                'password' => Hash::make($validated['password']),
+            ]);
+        }
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
